02306ffd05f553bae25401fa8211d6bb3c39740b
pilarmachinea <m.machinea@alumno.um.edu.ar>
2025-08-27 21:44:35 -0300

    player

--- core/Player.py
+++ core/Player.py
@@ -1,2 +1,10 @@
 class Player:
-    def __init__(self):
+    def __init__(self, color, nombre):
+        self.__color__ = color
+        self.__nombre__ = nombre
+
+    def obtener_color(self):
+        return self.__color__
+    
+    def obtener_nombre(self):
+        return self.__nombre__
