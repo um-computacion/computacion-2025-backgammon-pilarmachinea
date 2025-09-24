c4cb1d24c628abd645525722917adc3b82700dd1
pilarmachinea <m.machinea@alumno.um.edu.ar>
2025-09-24 20:03:59 -0300

    inicializando cli

---
+++ cli/__init__.py
@@ -0,0 +1,7 @@
+from core import BackgammonGame
+from core import Board
+from core import Checker
+from core import Dice
+from core import Player
+
+__all__ = ["BackgammonGame", "Board", "Checker", "Dice", "Player"]
